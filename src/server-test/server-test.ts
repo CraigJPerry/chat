00ceee4fcda30c9e8a945b27d64057402ae8f2ef
Craig J Perry <craigp84@gmail.com>
2016-04-13 00:49:16 +0100

    tests(integration): Basic integration test to validate configuration

--- src/server-test/server-test.ts
+++ src/server-test/server-test.ts
@@ -1,12 +1,14 @@
 /// <reference path="../../typings/main.d.ts" />
 
 import { expect } from 'chai';
-import { Placeholder } from "../server/server";
+import { app } from '../server/server';
+import * as http from 'http';
 
 describe('Placeholder', () => {
-    it('Should do something');
-    it('Should do something else too', () => {
-        let placeholder = new Placeholder();
-        expect(placeholder.doSomethingElse(2)).to.equal(4);
+    it('Should respond with status 200', done => {
+        http.get('http://127.0.0.1:' + app.get('port'), res => {
+            expect(res.statusCode).to.equal(200);
+            done();
+        });
     });
 });
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,12 @@
-export class Placeholder {
-    doSomethingElse(x: number): number {
-        return x * x;
-    }
-}
+import * as express from 'express';
+export let app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+let server = app.listen(app.get('port'), () => {
+    console.log(`Listening on port [${app.get('port')}]`);
+});
