c770798fd20d8aa809a9cb558d45ac02234f1aa8
Craig J Perry <craigp84@gmail.com>
2016-04-15 00:52:35 +0100

    tests(landing-page): Stub out initial acceptance criteria

--- src/server-test/server-test.ts
+++ src/server-test/server-test.ts
@@ -4,11 +4,17 @@ import { expect } from 'chai';
 import { app } from '../server/server';
 import * as http from 'http';
 
-describe('Placeholder', () => {
+describe('HTTP Status Code Is OK', () => {
     it('Should respond with status 200', done => {
         http.get('http://127.0.0.1:' + app.get('port'), res => {
             expect(res.statusCode).to.equal(200);
             done();
         });
     });
+});
+
+describe('Landing page is served', () => {
+    it('Should serve with an html mime type');
+    it('Should send a complete html page');
+    it('Should contain the chat mount point');
 });
