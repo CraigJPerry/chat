8f0b1046c48d7fa691a46700f36fff6564e8b297
Craig J Perry <craigp84@gmail.com>
2016-04-15 01:19:31 +0100

    tests(landing-page): Assert the landing page is served

--- src/server-test/server-test.ts
+++ src/server-test/server-test.ts
@@ -3,6 +3,7 @@
 import { expect } from 'chai';
 import { app } from '../server/server';
 import * as http from 'http';
+import * as supertest from 'supertest';
 
 describe('HTTP Status Code Is OK', () => {
     it('Should respond with status 200', done => {
@@ -14,7 +15,12 @@ describe('HTTP Status Code Is OK', () => {
 });
 
 describe('Landing page is served', () => {
-    it('Should serve with an html mime type');
+    it('Should serve with an html mime type', done => {
+        supertest(app)
+        .get('/')
+        .expect('Content-Type', /text\/html/)
+        .expect(200, done);
+    });
     it('Should send a complete html page');
     it('Should contain the chat mount point');
 });
